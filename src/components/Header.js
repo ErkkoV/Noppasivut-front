5740c6b3bc7bc225cb71160c494d7c5b9ddaf8a7
ErkkoV <erkko.vallivaara@gmail.com>
2022-08-22 15:53:51 +0300

    added navbar and navigation links with router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,25 @@
+import { Nav } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
 function Header() {
-    return <header>Test</header>;
+    const navigate = useNavigate();
+
+    return (
+        <Nav variant="pills" defaultActiveKey="/home">
+            <Nav.Item>
+                <Nav.Link onClick={() => navigate('/', { replace: true })}>Home</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link onClick={() => navigate('/DiceRoller', { replace: true })}>Dice Roller</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link onClick={() => navigate('/ProbCalc', { replace: true })}>Probability Calculator</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link onClick={() => navigate('/CharCreator', { replace: true })}>Character Creator</Nav.Link>
+            </Nav.Item>
+        </Nav>
+    );
 }
 
 export default Header;
