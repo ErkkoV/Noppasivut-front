1f6fe8c23067cc880d84f825d6384b1c3c484b4f
ErkkoV <erkko.vallivaara@gmail.com>
2022-08-23 08:16:01 +0300

    added navbar colours according to the tab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,63 @@
-import { Nav } from 'react-bootstrap';
+import { useState } from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 function Header() {
     const navigate = useNavigate();
 
+    const [activeKey, setActiveKey] = useState({ home: true, roller: false, prob: false, creator: false });
+
+    const setAct = (key) => {
+        const keys = activeKey;
+        keys.home = false;
+        keys.roller = false;
+        keys.prob = false;
+        keys.creator = false;
+        keys[key] = true;
+        setActiveKey(keys);
+    };
+
     return (
-        <Nav variant="pills" defaultActiveKey="/home">
-            <Nav.Item>
-                <Nav.Link onClick={() => navigate('/', { replace: true })}>Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => navigate('/DiceRoller', { replace: true })}>Dice Roller</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => navigate('/ProbCalc', { replace: true })}>Probability Calculator</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-                <Nav.Link onClick={() => navigate('/CharCreator', { replace: true })}>Character Creator</Nav.Link>
-            </Nav.Item>
-        </Nav>
+        <Navbar bg="dark" variant="dark">
+            <Nav>
+                <Nav.Link
+                    onClick={() => {
+                        navigate('/', { replace: false });
+                        setAct('home');
+                    }}
+                    active={activeKey.home}
+                >
+                    Home
+                </Nav.Link>
+                <Nav.Link
+                    onClick={() => {
+                        navigate('/DiceRoller', { replace: false });
+                        setAct('roller');
+                    }}
+                    active={activeKey.roller}
+                >
+                    Dice Roller
+                </Nav.Link>
+                <Nav.Link
+                    onClick={() => {
+                        navigate('/ProbCalc', { replace: false });
+                        setAct('prob');
+                    }}
+                    active={activeKey.prob}
+                >
+                    Probability Calculator
+                </Nav.Link>
+                <Nav.Link
+                    onClick={() => {
+                        navigate('/CharCreator', { replace: false });
+                        setAct('creator');
+                    }}
+                    active={activeKey.creator}
+                >
+                    Character Creator
+                </Nav.Link>
+            </Nav>
+        </Navbar>
     );
 }
 
