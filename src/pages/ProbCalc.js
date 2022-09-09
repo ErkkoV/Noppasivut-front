af8d26d0effef52bc109e636b1d3b4a2a8da22a4
ErkkoV <erkko.vallivaara@gmail.com>
2022-09-09 13:02:33 +0300

    adjusted values

--- src/pages/ProbCalc.js
+++ src/pages/ProbCalc.js
@@ -1,7 +1,7 @@
 import rollCalc from '../components/rollCalc';
 
 function ProbCalc() {
-    return <h1>{rollCalc(3, 9, 3, 9)}</h1>;
+    return <h1>{rollCalc(3, 3, 3, 3)}</h1>;
 }
 
 export default ProbCalc;
