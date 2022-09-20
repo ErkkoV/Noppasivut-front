5ce6dd7993d2d40d495820661931d95410c60caa
ErkkoV <erkko.vallivaara@gmail.com>
2022-09-20 07:32:50 +0300

    changed ip

--- src/socketio/connection.js
+++ src/socketio/connection.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const socket = io('ws://localhost:8000');
+const socket = io('ws://10.69.168.88:8000');
 
 const connectIo = () => {
     socket.on('connect', () => {
