aecf62e045066898985649cc12c00c5e888cc4ee
ErkkoV <erkko.vallivaara@gmail.com>
2022-09-20 12:28:06 +0300

    added simple chat

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,67 @@
+import { useState } from 'react';
+
+import { Form, Button } from 'react-bootstrap';
+
+import { socket } from '../socketio/connection';
+
 function Home() {
-    return <h1>Home</h1>;
+    const [name, setName] = useState('kahvikostaja');
+    const [message, setMessage] = useState();
+    const [messages, setMessages] = useState([]);
+
+    socket.on('messages-back', (args) => {
+        const messageList = messages;
+        if (messageList.length > 10) {
+            messageList.shift();
+        }
+        if (!messageList.includes(args)) {
+            messageList.push(args);
+            setMessages([...messageList]);
+        }
+    });
+
+    const sendMessage = () => {
+        socket.emit('messages-front', `${name}: ${message}`);
+    };
+
+    return (
+        <>
+            <Form onSubmit={(e) => e.preventDefault()}>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        id="name"
+                        required
+                        type="string"
+                        placeholder="kahvikostaja"
+                        defaultValue="kahvikostaja"
+                        onChange={(e) => {
+                            setName(e.target.value);
+                        }}
+                    />
+                </Form.Group>
+            </Form>
+            {messages.map((each) => (
+                <p key={each}>{each}</p>
+            ))}
+            <Form onSubmit={(e) => e.preventDefault()}>
+                <Form.Group>
+                    <Form.Label>Message</Form.Label>
+                    <Form.Control
+                        id="message"
+                        required
+                        type="string"
+                        placeholder=""
+                        defaultValue=""
+                        onChange={(e) => {
+                            setMessage(e.target.value);
+                        }}
+                    />
+                    <Button onClick={() => sendMessage()}>Send</Button>
+                </Form.Group>
+            </Form>
+        </>
+    );
 }
 
 export default Home;
