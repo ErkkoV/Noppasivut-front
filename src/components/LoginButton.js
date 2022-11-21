dfddf917fc6bd43f1db8ca7e5ce5d74ebc40d5a5
ErkkoV <erkko.vallivaara@gmail.com>
2022-11-21 09:45:20 +0200

    added loginmodal

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -1,20 +1,70 @@
-import { useContext } from 'react';
-import { Button } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+import { Button, Modal, Form } from 'react-bootstrap';
 
 import SocketContext from '../contexts/SocketContext';
 import { socket } from '../socketio/connection';
 
 function LoginButton() {
-    const { setUsedSocket } = useContext(SocketContext);
+    const { usedSocket, setUsedSocket } = useContext(SocketContext);
+
+    const [loginModal, setLoginModal] = useState(false);
+    const [newUser, setNewUser] = useState(false);
 
     const login = () => {
         setUsedSocket(socket('new user', 'new user'));
     };
 
     return (
-        <Button variant="info" onClick={() => login()}>
-            Login
-        </Button>
+        <>
+            <Button variant="info" onClick={() => setLoginModal(true)}>
+                Login
+            </Button>
+            {loginModal && (
+                <Modal show={loginModal} onHide={() => setLoginModal(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{newUser ? 'Create New User' : 'Login'}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form submit={(e) => e.preventDefault}>
+                            <Form.Group className="mb-3" controlId="formUser">
+                                <Form.Label>Username</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                        </Form>
+
+                        <Button
+                            variant="primary"
+                            onClick={() => {
+                                if (newUser) {
+                                    login();
+                                } else {
+                                    login();
+                                }
+                            }}
+                        >
+                            {newUser ? 'Create' : 'Login'}
+                        </Button>
+
+                        <Button
+                            onClick={() => {
+                                if (!newUser) {
+                                    setNewUser(true);
+                                } else {
+                                    setNewUser(false);
+                                }
+                            }}
+                        >
+                            {newUser ? 'Return to Login' : 'Create User'}
+                        </Button>
+                    </Modal.Body>
+                </Modal>
+            )}
+        </>
     );
 }
 
