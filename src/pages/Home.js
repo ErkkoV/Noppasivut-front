630df59cc50321210befabea75275ed45d30fdfb
ErkkoV <erkko.vallivaara@gmail.com>
2022-11-24 13:43:23 +0200

    added users for future use

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -46,56 +46,61 @@ function Home() {
     }, []);
 
     return (
-        <div style={{ 'margin-left': '20px', width: '90%' }}>
-            <p>Logged in as {user}</p>
-            <br />
-            {messages.map((each) => (
-                <p key={`${each.id}`}>
-                    {each.user}: {each.message}
-                </p>
-            ))}
-            <Form
-                onSubmit={(e) => e.preventDefault()}
-                onKeyPress={(e) => {
-                    if (e.key === 'Enter') {
-                        sendMessage();
-                    }
-                }}
-            >
-                <Form.Group>
-                    <Form.Label>Message {message ? `${message.length}/1000` : '0/1000'}</Form.Label>
-                    <Form.Control
-                        id="message"
-                        required
-                        type="string"
-                        placeholder=""
-                        value={message}
-                        onChange={(e) => {
-                            setMessage(e.target.value);
-                        }}
-                        isValid={message && message.length > 0 && message.length <= 1000}
-                        isInvalid={!message || message.length < 1 || message.length > 1000}
-                    />
-                    <Button
-                        onClick={() => {
+        <div style={{ overflow: 'hidden' }}>
+            <div style={{ 'margin-left': '20px', width: '65%', float: 'left' }}>
+                <p>Logged in as {user}</p>
+                <br />
+                {messages.map((each) => (
+                    <p key={`${each.id}`}>
+                        {each.user}: {each.message}
+                    </p>
+                ))}
+                <Form
+                    onSubmit={(e) => e.preventDefault()}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter') {
                             sendMessage();
-                        }}
-                    >
-                        Send
-                    </Button>
-                </Form.Group>
-            </Form>
-            {warning &&
-                (user === 'noppa' ||
-                    (message && message.length > 1000) ||
-                    (message && message.length < 1) ||
-                    !message) && (
-                    <Alert variant="danger">
-                        {message && message.length > 1000 && 'Please write message under 1000 characters.'}
-                        {!message || (message.length < 1 && 'Please write a message.')}
-                        {user === 'noppa' && 'Please log in or create a new user'}
-                    </Alert>
-                )}
+                        }
+                    }}
+                >
+                    <Form.Group>
+                        <Form.Label>Message {message ? `${message.length}/1000` : '0/1000'}</Form.Label>
+                        <Form.Control
+                            id="message"
+                            required
+                            type="string"
+                            placeholder=""
+                            value={message}
+                            onChange={(e) => {
+                                setMessage(e.target.value);
+                            }}
+                            isValid={message && message.length > 0 && message.length <= 1000}
+                            isInvalid={!message || message.length < 1 || message.length > 1000}
+                        />
+                        <Button
+                            onClick={() => {
+                                sendMessage();
+                            }}
+                        >
+                            Send
+                        </Button>
+                    </Form.Group>
+                </Form>
+                {warning &&
+                    (user === 'noppa' ||
+                        (message && message.length > 1000) ||
+                        (message && message.length < 1) ||
+                        !message) && (
+                        <Alert variant="danger">
+                            {message && message.length > 1000 && 'Please write message under 1000 characters.'}
+                            {!message || (message.length < 1 && 'Please write a message.')}
+                            {user === 'noppa' && 'Please log in or create a new user'}
+                        </Alert>
+                    )}
+            </div>
+            <div style={{ 'margin-left': '20px', width: '30%', float: 'left' }}>
+                <p>Users:</p>
+            </div>
         </div>
     );
 }
