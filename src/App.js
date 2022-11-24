6127bb78fdea75bb5c3fbf300fc1b0d4e5fda3d9
ErkkoV <erkko.vallivaara@gmail.com>
2022-11-24 08:41:50 +0200

    Updated router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState, useMemo } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useState, useMemo } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import LoadSpinner from './utils/LoadSpinner';
 import Base from './pages/Base';
+import Diceroller from './pages/Diceroller';
+import ProbCalc from './pages/ProbCalc';
+import Home from './pages/Home';
+import CharCreator from './pages/CharCreator';
+
 import { socket, connectIo } from './socketio/connection';
 import SocketContext from './contexts/SocketContext';
-
-const Diceroller = React.lazy(() => import('./pages/Diceroller'));
-const ProbCalc = React.lazy(() => import('./pages/ProbCalc'));
-const Home = React.lazy(() => import('./pages/Home'));
-const CharCreator = React.lazy(() => import('./pages/CharCreator'));
 
 function App() {
     const [usedSocket, setUsedSocket] = useState(socket('noppa', 'noppa'));
@@ -35,44 +35,34 @@ function App() {
         localStorage.probs = JSON.stringify(list);
     });
 
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Base />,
+            children: [
+                {
+                    path: '/',
+                    element: <Home />,
+                },
+                {
+                    path: 'DiceRoller',
+                    element: <Diceroller />,
+                },
+                {
+                    path: 'ProbCalc',
+                    element: <ProbCalc />,
+                },
+                {
+                    path: 'CharCreator',
+                    element: <CharCreator />,
+                },
+            ],
+        },
+    ]);
+
     return (
         <SocketContext.Provider value={value}>
-            <Routes>
-                <Route path="/" element={<Base />}>
-                    <Route
-                        path="/"
-                        element={
-                            <React.Suspense fallback={<LoadSpinner />}>
-                                <Home />
-                            </React.Suspense>
-                        }
-                    />
-                    <Route
-                        path="/Diceroller"
-                        element={
-                            <React.Suspense fallback={<LoadSpinner />}>
-                                <Diceroller />
-                            </React.Suspense>
-                        }
-                    />
-                    <Route
-                        path="/ProbCalc"
-                        element={
-                            <React.Suspense fallback={<LoadSpinner />}>
-                                <ProbCalc />
-                            </React.Suspense>
-                        }
-                    />
-                    <Route
-                        path="/CharCreator"
-                        element={
-                            <React.Suspense fallback={<LoadSpinner />}>
-                                <CharCreator />
-                            </React.Suspense>
-                        }
-                    />
-                </Route>
-            </Routes>
+            <RouterProvider router={router} fallbackElement={<LoadSpinner />} />
         </SocketContext.Provider>
     );
 }
