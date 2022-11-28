1623afaa612ff65c4f62d652ffd768dd4091f011
ErkkoV <erkko.vallivaara@gmail.com>
2022-11-28 11:20:17 +0200

    working session

--- src/components/SessionMenu.js
+++ src/components/SessionMenu.js
@@ -1,6 +1,10 @@
 import { useContext } from 'react';
 
+import SessionContext from '../contexts/SessionContext';
+
 function SessionMenu() {
+    const { session } = useContext(SessionContext);
+
     return <h3 style={{ color: 'white', 'margin-left': '50px', 'margin-top': '2px' }}>{session} &nbsp;</h3>;
 }
 
