e8b7c9a5ac8bc84afd6c1a70a879bafdd64b185f
ErkkoV <erkko.vallivaara@gmail.com>
2022-11-29 12:03:10 +0200

    allUsers

--- src/components/UserWindow.js
+++ src/components/UserWindow.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Button } from 'react-bootstrap';
 
 import SocketContext from '../contexts/SocketContext';
@@ -12,12 +12,20 @@ function UserWindow() {
     const { session } = useContext(SessionContext);
     const { users } = useContext(UsersContext);
 
+    const [allUsers, setAllUsers] = useState([]);
+
     const inviteUser = (inv) => {
         usedSocket.emit('invite', { session, user, inv });
     };
 
+    usedSocket.on('all-users', (args) => {
+        console.log(args);
+        setAllUsers([...args]);
+    });
+
     return (
         <>
+            {allUsers}
             <Button variant="success" onClick={() => inviteUser('tonipal')}>
                 Invite Users
             </Button>
